aa16b35a3e966fac85689a1a4ff2d35f1ba009bd
cbarbour-edu <cmbarbour@uncg.edu>
2025-10-07 11:14:19 -0400

    Implemented methods for the pet repository. Includes methods to find pets by awards, species, and names. Implemented methods for the pet service, including getting pets by their ID, awards, species, and name. Methods also include getting all pets, adding new pets, updating existing pets, and deleting pets by their ID.

--- src/main/java/com/example/CRUD/pet/PetRepository.java
+++ src/main/java/com/example/CRUD/pet/PetRepository.java
@@ -9,4 +9,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
     
+    List<Pet> getPetsBySpecies(String species);
+
+    @Query(value = "select * from pets p where p.awards like %?1%", nativeQuery = true)
+    List<Pet> getPetsByAwards(String award);
+
+    @Query(value = "select * from pets p where p.name like %?1%", nativeQuery = true)
+    List<Pet> getPetsByName(String name);
 }
--- src/main/java/com/example/CRUD/pet/PetService.java
+++ src/main/java/com/example/CRUD/pet/PetService.java
@@ -1,15 +1,91 @@
 package com.example.CRUD.pet;
 
-import java.io.IOException;
-import java.io.File;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Service
 public class PetService {
+    @Autowired
+    private PetRepository petRepository;
+
+    /**
+     * Method to get all pets
+     *
+     * @return List of all pets
+     */
+    public Object getAllPets() {
+        return petRepository.findAll();
+    }
+
+    /**
+     * Method to get a pet by its ID
+     *
+     * @param petID ID of the pet
+     * @return The pet with the given ID
+     */
+    public Object getPetsByID(@PathVariable Long ID) {
+        return petRepository.findById(ID).orElse(null);
+    }
+
+    /**
+     * Method to get a pet by its name
+     *
+     * @param name The name of the pet
+     * @return List of pets with the given name
+     */
+    public Object getPetsByName(@PathVariable String name) {
+        return petRepository.getPetsByName(name);
+    }
+
+    /**
+     * Method to get a pet by its species
+     *
+     * @param species The species of the pet
+     * @return List of pets with the given species
+     */
+    public Object getPetsBySpecies(@PathVariable String species) {
+        return petRepository.getPetsBySpecies(species);
+    }
+
+    /**
+     * Method to a pet by its awards
+     *
+     * @param award The award of the pet
+     * @return List of pets with the given award
+     */
+    public Object getPetsByAward(@PathVariable String award) {
+        return petRepository.getPetsByAwards(award);
+    }
+
+    /**
+     * Method to add a new pet
+     *
+     * @param pet The pet to be added
+     * @return The pet that was added
+     */
+    public Pet addPet(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    /**
+     * Method to change an existing pet
+     *
+     * @param petID The ID of the pet to be changed
+     * @param pet The pet with the updated information
+     * @return The pet that was updated
+     */
+    public Pet updatePet(Long petID, Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    /**
+     * Method to delete a pet by its ID
+     *
+     * @param petID The ID of the pet to be deleted
+     */
+    public void deletePet(Long petID) {
+        petRepository.deleteById(petID);
+    }
     
 }
